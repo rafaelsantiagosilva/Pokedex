c6a2e447b196b2336d1b6efa5e2500021bde1fc8
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2024-01-25 16:23:52 -0300

    Lendo os tipos de pokemon a partir da API

--- src/scripts/dist/main.js
+++ src/scripts/dist/main.js
@@ -1,11 +1,6 @@
 "use strict";
-let pokemonId = 1;
-let pokeapiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
-const POKEMON_IMAGE = document.getElementById("pokemon-image");
-const POKEMON_NAME_DOM = document.getElementById("pokemon-name");
-const POKEMON_ID_DOM = document.getElementById("pokemon-id");
 class Converter {
-    pokemonNameFirstLetterToUpperCase(pokemonName) {
+    firstLetterOfStringToUpperCase(pokemonName) {
         let name = pokemonName[0].toUpperCase();
         name += pokemonName.slice(1, pokemonName.length).toLowerCase();
         return name;
@@ -26,22 +21,46 @@ class Converter {
         return id;
     }
 }
+let pokemonId = 67;
+let pokeapiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
+const POKEMON_IMAGE = document.getElementById("pokemon-image");
+const POKEMON_NAME_DOM = document.getElementById("pokemon-name");
+const POKEMON_ID_DOM = document.getElementById("pokemon-id");
+const POKEMON_TYPE_LIST_DOM = document.getElementById("pokemon-types");
+const converter = new Converter();
 function modifyPokeApiUrl(step) {
     pokemonId += step;
     pokeapiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
+}
+function addTypesToListDom(types) {
+    types.forEach((type) => {
+        // @ts-ignore
+        let typeName = type.type.name;
+        let listItem = document.createElement("li");
+        listItem.classList.add("pokemon-type");
+        listItem.classList.add(typeName);
+        let text = document.createTextNode(converter.firstLetterOfStringToUpperCase(typeName));
+        listItem.appendChild(text);
+        // @ts-ignore
+        POKEMON_TYPE_LIST_DOM.appendChild(listItem);
+    });
 }
 function loadPokemon() {
     fetch(pokeapiUrl)
         .then((response) => response.json())
         .then((response) => {
-        const converter = new Converter();
-        POKEMON_NAME_DOM.innerText = converter.pokemonNameFirstLetterToUpperCase(response.name);
-        POKEMON_ID_DOM.innerText = converter.pokemonAddZerosAndHashtagToId(response.id);
-        POKEMON_IMAGE.setAttribute("src", response.sprites.other.dream_world.front_default);
-    })
+            // @ts-ignore
+            POKEMON_NAME_DOM.innerText = converter.firstLetterOfStringToUpperCase(response.name);
+            // @ts-ignore
+            POKEMON_ID_DOM.innerText = converter.pokemonAddZerosAndHashtagToId(response.id);
+            // @ts-ignore
+            POKEMON_IMAGE.setAttribute("src", response.sprites.other.dream_world.front_default);
+            addTypesToListDom(response.types);
+        })
         .catch((error) => {
-        console.error(error);
-    });
+            console.error(error);
+        });
 }
+// @ts-ignore
 document.addEventListener("onload", loadPokemon());
 //# sourceMappingURL=main.js.map
--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,19 +1,7 @@
 "use strict";
 
-let pokemonId: number = 1;
-let pokeapiUrl: string = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
-
-const POKEMON_IMAGE: HTMLElement | null =
-	document.getElementById("pokemon-image");
-
-const POKEMON_NAME_DOM: HTMLElement | null =
-	document.getElementById("pokemon-name");
-
-const POKEMON_ID_DOM: HTMLElement | null =
-	document.getElementById("pokemon-id");
-
 class Converter {
-	public pokemonNameFirstLetterToUpperCase(pokemonName: string): string {
+	public firstLetterOfStringToUpperCase(pokemonName: string): string {
 		let name: string = pokemonName[0].toUpperCase();
 		name += pokemonName.slice(1, pokemonName.length).toLowerCase();
 		return name;
@@ -35,18 +23,53 @@ class Converter {
 	}
 }
 
+let pokemonId: number = 1;
+let pokeapiUrl: string = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
+
+const POKEMON_IMAGE: HTMLElement | null =
+	document.getElementById("pokemon-image");
+
+const POKEMON_NAME_DOM: HTMLElement | null =
+	document.getElementById("pokemon-name");
+
+const POKEMON_ID_DOM: HTMLElement | null =
+	document.getElementById("pokemon-id");
+
+const POKEMON_TYPE_LIST_DOM: HTMLElement | null =
+	document.getElementById("pokemon-types");
+
+const converter: Converter = new Converter();
+
 function modifyPokeApiUrl(step: number) {
 	pokemonId += step;
 	pokeapiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
+}
+
+function addTypesToListDom(types: []) {
+	types.forEach((type) => {
+		// @ts-ignore
+		let typeName = type.type.name;
+
+		let listItem = document.createElement("li");
+		listItem.classList.add("pokemon-type");
+		listItem.classList.add(typeName);
+
+		let text = document.createTextNode(
+			converter.firstLetterOfStringToUpperCase(typeName)
+		);
+		listItem.appendChild(text);
+
+		// @ts-ignore
+		POKEMON_TYPE_LIST_DOM.appendChild(listItem);
+	});
 }
 
 function loadPokemon() {
 	fetch(pokeapiUrl)
 		.then((response) => response.json())
 		.then((response) => {
-			const converter: Converter = new Converter();
 			// @ts-ignore
-			POKEMON_NAME_DOM.innerText = converter.pokemonNameFirstLetterToUpperCase(
+			POKEMON_NAME_DOM.innerText = converter.firstLetterOfStringToUpperCase(
 				response.name
 			);
 			// @ts-ignore
@@ -58,6 +81,7 @@ function loadPokemon() {
 				"src",
 				response.sprites.other.dream_world.front_default
 			);
+			addTypesToListDom(response.types);
 		})
 		.catch((error) => {
 			console.error(error);
